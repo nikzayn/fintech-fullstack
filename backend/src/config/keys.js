ea98a3e1fba3eff9e20fcd82e03b9c70a01b7f90
Nikhil Vaidyar <nikhilvaidyar1997@gmail.com>
2020-07-23 01:04:46 +0530

    keys config --removed

--- backend/src/config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`
-}
